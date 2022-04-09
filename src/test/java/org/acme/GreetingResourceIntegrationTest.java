943c5158e61aba349602356e9cce008baa9a57cd
Eugen Blattner <eugen.blattner@mayflower.de>
2022-04-09 11:16:50 +0200

    initial commit

---
+++ src/test/java/org/acme/GreetingResourceIntegrationTest.java
@@ -0,0 +1,9 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIntegrationTest extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
