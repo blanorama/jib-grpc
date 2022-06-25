ef8cd5c02cec196512ce1ddfc4670156fa84efe0
lecodeski <e.blattner@web.de>
2022-06-26 01:41:40 +0200

    del: remove integration test to stay focused on containerization and JIB

--- src/test/java/org/acme/GreetingSlimIntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package org.acme;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingSlimIntegrationTest extends GreetingSlimTest {
-
-  // Execute the same tests but in native mode.
-}
