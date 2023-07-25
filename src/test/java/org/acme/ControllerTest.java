6ba6fc3c374d9287eeb13c47a3fb1c52dbf5a831
Big Lecodeski <22894442+lecodeski@users.noreply.github.com>
2023-07-25 14:43:04 +0200

    mod: proper naming for Controller

--- src/test/java/org/acme/ControllerTest.java
+++ src/test/java/org/acme/ControllerTest.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingSlimTest {
+public class ControllerTest {
 
   @Test
   public void testHelloEndpoint() {
