546e94ccbe37d0c00a9a1b0196d33e27dca2e59d
Big Lecodeski <22894442+lecodeski@users.noreply.github.com>
2023-07-25 12:20:27 +0200

    mod: improve logging

--- src/main/java/org/acme/HelloSlimService.java
+++ src/main/java/org/acme/HelloSlimService.java
@@ -9,7 +9,7 @@ public class HelloSlimService implements SlimGrpc {
 
   @Override
   public Uni<SlimReply> helloSlim(final SlimRequest request) {
-    Log.info("Processing gRPC Request 🤘");
+    Log.infof("Processing REST request 🤘for %s", request.getName());
     return Uni.createFrom().item(
         "Hi, your Name is what? Your name is who? Your name is: Slim" + request.getName() + "!"
     ).map(msg -> SlimReply.newBuilder().setMessage(msg).build());
